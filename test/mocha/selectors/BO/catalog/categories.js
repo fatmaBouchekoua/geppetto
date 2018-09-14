b0fd170b629abf32a08af12e915961690aa6a2aa
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2018-09-14 16:04:29 +0100

    TE:PR-10291

---
+++ test/mocha/selectors/BO/catalog/categories.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Categories: {
+    edit_home_button: '#content > div.row > div > ul > li > a',
+    input_group: '#fieldset_0 div:nth-child(%D) div.translatable-field.lang-1 div.input-group'
+  }
+};
